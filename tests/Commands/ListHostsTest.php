cd4db4ce0a49386fdfcd7c8c91d6a755d1999ab4
Alex Vanderbist <alex.vanderbist@gmail.com>
2017-02-24 11:58:17 +0100

    Added tests for list hosts command

---
+++ tests/Commands/ListHostsTest.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace Spatie\ServerMonitor\Test\Commands;
+
+use Artisan;
+use Spatie\ServerMonitor\Models\Check;
+use Spatie\ServerMonitor\Models\Enums\CheckStatus;
+use Spatie\ServerMonitor\Models\Host;
+use Spatie\ServerMonitor\Test\TestCase;
+
+class ListHostsTest extends TestCase
+{
+    protected $consoleOutputCache;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->consoleOutputCache = null;
+    }
+
+    /** @test */
+    public function it_displays_a_message_when_no_hosts_are_configures()
+    {
+        Artisan::call('server-monitor:list');
+
+        $this->dontSeeInConsoleOutput(['Checks', 'Health']);
+
+        $this->seeInConsoleOutput('There are no hosts configured');
+    }
+
+    /** @test */
+    public function it_displays_a_list_of_all_hosts()
+    {
+        $testHosts = ['test-host-1', 'test-host-2'];
+
+        $this->addHosts($testHosts);
+
+        Artisan::call('server-monitor:list');
+
+        $this->dontSeeInConsoleOutput('There are no hosts configured');
+
+        $this->seeInConsoleOutput($testHosts);
+    }
+
+    /** @test */
+    public function it_filters_all_hosts_by_hostname()
+    {
+        $testHosts = ['test-host-1', 'test-host-2'];
+
+        $this->addHosts($testHosts);
+
+        Artisan::call('server-monitor:list', ['--host' => $testHosts[0]]);
+
+        $this->seeInConsoleOutput($testHosts[0]);
+
+        $this->dontSeeInConsoleOutput($testHosts[1]);
+    }
+
+    /** @test */
+    public function it_filters_all_hosts_by_checktype()
+    {
+        $this->addHosts('test-host', ['correct-check', 'wrong-check']);
+
+        Artisan::call('server-monitor:list', ['--check' => 'correct-check']);
+
+        $this->dontSeeInConsoleOutput('wrong-check');
+
+        $this->seeInConsoleOutput('correct-check');
+    }
+
+    /** @test */
+    public function it_filters_all_hosts_by_hostname_and_checktype()
+    {
+        $this->addHosts('wrong-host');
+        $this->addHosts('correct-host', ['wrong-check', 'correct-check']);
+
+        Artisan::call('server-monitor:list', [
+            '--host' => 'correct-host',
+            '--check' => 'correct-check',
+        ]);
+
+        $this->dontSeeInConsoleOutput(['wrong-host', 'wrong-check']);
+
+        $this->seeInConsoleOutput(['correct-check', 'correct-host']);
+    }
+
+    public function addHosts($names, array $checks = [])
+    {
+        if (! is_array($names)) {
+            $names = [$names];
+        }
+
+        collect($names)->each(function ($name) use ($checks) {
+            Host::create([
+                'name' => $name,
+                'ssh_user' => 'user',
+                'port' => 22,
+            ])->checks()->saveMany(collect($checks)->map(function (string $checkName) {
+                return new Check([
+                    'type' => $checkName,
+                    'status' => CheckStatus::NOT_YET_CHECKED,
+                    'custom_properties' => [],
+                ]);
+            }));
+        });
+    }
+
+    /**
+     * @param string|array $searchStrings
+     */
+    protected function seeInConsoleOutput($searchStrings)
+    {
+        if (! is_array($searchStrings)) {
+            $searchStrings = [$searchStrings];
+        }
+        $output = $this->getArtisanOutput();
+        foreach ($searchStrings as $searchString) {
+            $this->assertContains((string) $searchString, $output);
+        }
+    }
+
+    /**
+     * @param string|array $searchStrings
+     */
+    protected function dontSeeInConsoleOutput($searchStrings)
+    {
+        if (! is_array($searchStrings)) {
+            $searchStrings = [$searchStrings];
+        }
+        $output = $this->getArtisanOutput();
+        foreach ($searchStrings as $searchString) {
+            $this->assertNotContains((string) $searchString, $output);
+        }
+    }
+
+    protected function getArtisanOutput(): string
+    {
+        $this->consoleOutputCache .= Artisan::output();
+        return $this->consoleOutputCache;
+    }
+}
