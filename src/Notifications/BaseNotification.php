13fea08489ccd3885abef43589b14de853e96a46
freek <freek@spatie.be>
2017-03-01 23:08:01 +0100

    commit

--- src/Notifications/BaseNotification.php
+++ src/Notifications/BaseNotification.php
@@ -25,7 +25,7 @@ abstract class BaseNotification extends Notification
 
     protected function getMessageText(): ?string
     {
-        return ucfirst($this->getCheck()->message);
+        return ucfirst($this->getCheck()->last_run_message);
     }
 
     protected function getSubject(): string
