173e497906adc943ef4ff8a183748604f1acb8fa
can <canazizoglu97@gmail.com>
2018-06-18 03:11:55 +0300

    excluded errors added

---
+++ src/Excluded/ExcludedErrors.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dd
+ * Date: 18.06.2018
+ * Time: 02:52
+ */
+
+namespace Spatie\ServerMonitor\Excluded;
+
+
+use phpDocumentor\Reflection\Types\Boolean;
+
+class ExcludedErrors
+{
+
+
+
+    public static function getExcludedErrors() : array
+    {
+
+        return config("server-monitor.excluded_errors");
+
+    }
+
+
+    public static function hasExcludedError(String $errorString) : Bool
+    {
+
+        $excludedErrors = self::getExcludedErrors();
+
+
+
+        foreach ($excludedErrors as $excludedError)
+        {
+            $counter=0;
+            str_replace($excludedError,"_REPLACE_",$errorString,$counter);
+            if($counter>0) return false;
+        }
+
+        return true;
+
+    }
+
+
+}
