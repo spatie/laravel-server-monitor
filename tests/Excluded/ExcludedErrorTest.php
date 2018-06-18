23d44f1e3c77493415c6cf7b52ef0b48452ff25a
can <canazizoglu97@gmail.com>
2018-06-18 13:14:35 +0300

    ExcludedErrorTest added

---
+++ tests/Excluded/ExcludedErrorTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Spatie\ServerMonitor\Test\Excluded;
+
+use Spatie\ServerMonitor\Excluded\ExcludedErrors;
+use Spatie\ServerMonitor\Models\Check;
+use Symfony\Component\Process\Process;
+use Spatie\ServerMonitor\Test\TestCase;
+use Spatie\ServerMonitor\Manipulators\Manipulator;
+
+class ExcludedErrorTest extends TestCase
+{
+
+    /** @test */
+    public function testExcludedError()
+    {
+
+        //pretending like a error comes from SSH connection because i dont know how to emulate this situation
+        $error = "POSSIBLE BREAK-IN ATTEMPT!";
+
+
+
+        if(ExcludedErrors::hasExcludedError($error))
+        {
+            $this->assertTrue(true);
+            return;
+        }
+
+        $this->assertTrue(false);
+
+    }
+
+}
