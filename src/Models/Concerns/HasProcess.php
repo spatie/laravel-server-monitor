e6f7dafd3e9cb5308e8e9c5ae74f668c46fdd23f
Tjitse Efde <Tjitse@vendic.nl>
2018-07-02 14:04:05 +0200

    Move sshCommandPrefix above sshCommandSuffix

--- src/Models/Concerns/HasProcess.php
+++ src/Models/Concerns/HasProcess.php
@@ -28,8 +28,8 @@ trait HasProcess
 
         $portArgument = empty($this->host->port) ? '' : "-p {$this->host->port}";
 
-        $sshCommandSuffix = config('server-monitor.ssh_command_suffix');
         $sshCommandPrefix = config('server-monitor.ssh_command_prefix');
+        $sshCommandSuffix = config('server-monitor.ssh_command_suffix');
 
         return "ssh {$sshCommandPrefix} {$this->getTarget()} {$portArgument} {$sshCommandSuffix} 'bash -se <<$delimiter".PHP_EOL
             .'set -e'.PHP_EOL
